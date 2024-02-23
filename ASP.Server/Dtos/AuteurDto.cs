702cd2b16f95b5a5620af7b37b046892e9bfac91
Yehoudi <vincentyehoudi7@gmail.com>
2024-02-23 10:12:07 +0100

    Changement dans l'API

---
+++ ASP.Server/Dtos/AuteurDto.cs
@@ -0,0 +1,14 @@
+﻿using ASP.Server.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.Server.Dtos
+{
+    public class AuteurDto
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+    }
+}
--- ASP.Server/Dtos/BookWithoutContentDto.cs
+++ ASP.Server/Dtos/BookWithoutContentDto.cs
@@ -3,12 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ASP.Server.Dtos
 {
-    public class BookDto
+    public class BookWithoutContentDto
     {
         public int Id { get; set; }
         public string Nom { get; set; }
         public double Prix { get; set; }
-        public string Contenu { get; set; }
         public List<GenreDTo> Genres { get; set; } 
         public List<AuteurDto> Auteurs { get; set; }
 
